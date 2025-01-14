30edf83e513bd8945ebb9c84b7a8034df90f7197
William Lathrop <willguy21@gmail.com>
2025-01-13 21:11:27 -0800

    Add Initial Project Structure

---
+++ AOAHub.Db/AOAContext.cs
@@ -0,0 +1,12 @@
+﻿using AOAHub.Db.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AOAHub.Db
+{
+    public class AOAContext : DbContext, IAOAContext
+    {
+        protected AOAContext()
+        {
+        }
+    }
+}
